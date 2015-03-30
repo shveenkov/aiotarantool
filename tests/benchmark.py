a7742236c584671288f5ac102735a3f2d3c225ac
Dmitriy Shveenkov <shveenkov@mail.ru>
2015-03-30 18:48:54 +0300

    fix benchmark

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -35,6 +35,16 @@ for it in range(100000):
 t2 = time.time()
 benchmark["tarantool"]["select"] = t2 - t1
 
+# update test
+print("tarantool update test")
+t1 = time.time()
+for it in range(100000):
+    r = tnt.update("tester", it, [("=", 2, it)])
+
+
+t2 = time.time()
+benchmark["tarantool"]["update"] = t2 - t1
+
 # delete test
 print("tarantool delete test")
 t1 = time.time()
@@ -65,6 +75,15 @@ def select_job(tnt):
     for i in range(2500):
         cnt += 1
         yield from tnt.select("tester", cnt)
+
+
+@asyncio.coroutine
+def update_job(tnt):
+    global cnt
+
+    for i in range(2500):
+        cnt += 1
+        yield from tnt.update("tester", cnt, [("=", 2, cnt)])
 
 
 @asyncio.coroutine
@@ -102,6 +121,17 @@ loop.run_until_complete(asyncio.wait(tasks))
 t2 = loop.time()
 benchmark["aiotarantool"]["select"] = t2 - t1
 
+# update test
+print("aiotarantool update test")
+t1 = loop.time()
+cnt = 0
+tasks = [asyncio.async(update_job(tnt))
+         for _ in range(40)]
+
+loop.run_until_complete(asyncio.wait(tasks))
+t2 = loop.time()
+benchmark["aiotarantool"]["update"] = t2 - t1
+
 # delete test
 print("aiotarantool delete test")
 t1 = loop.time()
@@ -117,5 +147,5 @@ loop.close()
 
 print("\nbenchmark results:")
 print("call    tarantool  aiotarantool")
-for k in ("insert", "select", "delete"):
+for k in ("insert", "select", "update", "delete"):
     print("{2:6}: {0:0.6f}  {1:0.6f}".format(benchmark["tarantool"][k], benchmark["aiotarantool"][k], k))
